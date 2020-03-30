2f0429bfb20e481399c50381a92c9aa270dd8025
coma64 <coma64@outlook.com>
2020-03-30 16:52:23 +0200

    Apply clipboard patch to use CLIPBOARD clipboard and not PRIMARY

--- x.c
+++ x.c
@@ -661,6 +661,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
@@ -681,6 +683,8 @@ brelease(XEvent *e)
 		return;
 	if (e->xbutton.button == Button1)
 		mousesel(e, 1);
+    else if (e->xbutton.button == Button2)
+        selpaste(NULL);
     else if (e->xbutton.button == Button3)
         selpaste(NULL);
 }
